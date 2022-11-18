fe17a8213c56947717fea1ff078115334b88ce40
Stephany Ramirez <stephany.ramireze@gmail.com>
2022-11-18 03:06:19 -0700

    making the page more responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,11 +16,12 @@ function About() {
       <div className='text-img'>
         <div className='about-text'>
           <h1>Quienes somos</h1>
-          <p> Nuestra empresa es originaria de Tequixquiac, estado de México nacida en el 2004 para dar servicios de demolición e instalación de materiales refractarios, siguiendo el legado de nuestras generaciones, desde entonces nos hemos expandido para dar servicioH de mantenimiento industrial y civil. <br /><br />
+          <p> Nuestra empresa es originaria de Tequixquiac, estado de México nacida en el 2004 para dar servicios de demolición e instalación de materiales refractarios, siguiendo el legado de nuestras generaciones, desde entonces nos hemos expandido para dar servicios de mantenimiento industrial y civil. <br /><br />
               Contamos con toda la infraestructura, herramientas, equipo y personal técnico calificado para cumplir con los requerimiento y compromisos de nuestros clientes.<br /><br />
               Estamos comprometidos a satisfacer todas las necesidades de nuestros clientes, asegurándonos de la solidez de nuestros procesos y dándoles la confianza de una empresa que certifica y se mantiene.</p>
+          <div ref={ref} className={inView ? 'about-img-activo' : 'about-img'}/>
         </div>
-        <img src="images/about_2.jpeg" alt="quienes somos" ref={ref} className={inView ? 'about-img-activo' : 'about-img'}/>
+
       </div>
     </div>
   )
